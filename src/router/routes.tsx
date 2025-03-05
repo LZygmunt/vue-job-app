5c701283040e03a074762ba3c7af60f2c9438467
LZygmunt <lzygmunt0@gmail.com>
2025-03-05 17:09:08 +0100

    feat: add jobs page

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,9 +1,15 @@
 import HomeView from '#/views/HomeView.vue'
+import JobsView from '#/views/JobsView.vue'
 
 export const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: JobsView,
   },
 ]
---
+++ src/views/JobsView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import JobBrowseSection from '#/components/JobBrowseSection.vue'
+import { ref } from 'vue'
+import jsonJobs from '../../jobs.json'
+
+const jobs = ref(jsonJobs.jobs)
+</script>
+
+<template>
+  <JobBrowseSection :jobs="jobs" />
+</template>
+
+<style scoped></style>
