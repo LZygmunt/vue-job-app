d84b626566b0582c1d16a2fd7ae3f8e9ca27cc00
LZygmunt <lzygmunt0@gmail.com>
2025-03-05 10:02:52 +0100

    refactor: move HomeView component directly to views dir

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,44 @@
 <script setup lang="ts">
 import ActionCard from '#/components/ActionCard.vue'
 import BaseBanner from '#/components/BaseBanner.vue'
+import BaseRedirectButton from '#/components/BaseRedirectButton.vue'
 </script>
 
 <template>
   <main
     id="home-view"
     title="Home"
-    role="main"
   >
     <BaseBanner
       title="Become a Vue dev"
       desc="Find the Vue job that fills your skills and needs"
     />
-    <div class="w-full max-w-7/8 mx-auto my-6 grid grid-cols-2 gap-4">
+    <section class="w-full max-w-7/8 mx-auto my-6 grid grid-cols-2 gap-4">
       <ActionCard
         class="p-6"
         title="For developers"
         description="Browse our Vue jobs and start your career today"
-        redirectLabel="Browse Jobs"
-        redirectTo="/jobs"
         variant="secondary"
-      />
+      >
+        <BaseRedirectButton
+          label="Browse Jobs"
+          redirectTo="/jobs"
+          variant="secondary"
+        />
+      </ActionCard>
       <ActionCard
         class="p-6"
         title="For Employers"
         description="List your job to find the perfect developer for the role "
-        redirectLabel="Add Job"
-        redirectTo="/jobs/add"
-      />
-    </div>
+      >
+        <BaseRedirectButton
+          label="Add Job"
+          redirectTo="/jobs/add"
+        />
+      </ActionCard>
+    </section>
+    <section>
+      <h2>Browse Jobs</h2>
+    </section>
   </main>
 </template>
