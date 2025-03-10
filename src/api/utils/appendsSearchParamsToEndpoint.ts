1614322f64d700131bdd2925f825f56f4d6e84a7
LZygmunt <lzygmunt0@gmail.com>
2025-03-10 21:34:03 +0100

    refactor(api/appendsSearchParamsToEndpoint): change named export to default

--- src/api/utils/appendsSearchParamsToEndpoint.ts
+++ src/api/utils/appendsSearchParamsToEndpoint.ts
@@ -1,7 +1,9 @@
-export const appendsSearchParamsToEndpoint = ( endpoint: string, searchParams?: string ) => {
+const appendsSearchParamsToEndpoint = ( endpoint: string, searchParams?: string ) => {
   if ( !searchParams ) {
     return endpoint
   }
 
   return `${ endpoint }?${ searchParams }`
 }
+
+export default appendsSearchParamsToEndpoint
