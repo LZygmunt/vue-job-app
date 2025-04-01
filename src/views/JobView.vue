3563596ba0929255d31407eab2e08ab751cccd25
LZygmunt <lzygmunt0@gmail.com>
2025-04-01 17:08:53 +0200

    fix(JobView): set min width for first column

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -18,41 +18,96 @@ const { data, isLoading } = useGetJobById({ jobId: params.id as string })
   <section class="details">
     <JobCardWrapper class="details__main">
       <template #mainDetails>
-        <div v-if="isLoading" class="h-2 w-1/5 rounded bg-stone-400" />
+        <div
+          v-if="isLoading"
+          class="h-2 w-1/5 rounded bg-stone-400"
+        />
         <span v-else>{{ data?.type }}</span>
-        <div v-if="isLoading" class="h-4 w-3/5 rounded bg-stone-400" />
-        <span v-else class="font-bold text-4xl">{{ data?.title }}</span>
-        <div v-if="isLoading" class="h-2 w-28 rounded bg-stone-400" />
+        <div
+          v-if="isLoading"
+          class="h-4 w-3/5 rounded bg-stone-400"
+        />
+        <span
+          v-else
+          class="font-bold text-4xl"
+          >{{ data?.title }}</span
+        >
+        <div
+          v-if="isLoading"
+          class="h-2 w-28 rounded bg-stone-400"
+        />
         <JobLocation v-else>{{ data?.location }}</JobLocation>
       </template>
     </JobCardWrapper>
     <JobCardWrapper class="details__company">
       <template #mainDetails>
         <span class="font-bold text-xl">Company Info</span>
-        <div v-if="isLoading" class="h-2 w-28 rounded bg-stone-400" />
-        <span v-else class="font-bold text-2xl">{{ data?.company?.name }}</span>
-        <div v-if="isLoading" class="h-2 w-28 rounded bg-stone-400" />
-        <span v-else class="text-lg">{{ data?.company?.description }}</span>
+        <div
+          v-if="isLoading"
+          class="h-2 w-28 rounded bg-stone-400"
+        />
+        <span
+          v-else
+          class="font-bold text-2xl"
+          >{{ data?.company?.name }}</span
+        >
+        <div
+          v-if="isLoading"
+          class="h-2 w-28 rounded bg-stone-400"
+        />
+        <span
+          v-else
+          class="text-lg"
+          >{{ data?.company?.description }}</span
+        >
       </template>
       <template #footer>
         <p class="flex flex-col gap-2">
           <span class="font-bold text-2xl">Contact Email:</span>
-          <span v-if="isLoading" class="h-2 w-28 rounded bg-stone-400" />
-          <span v-else class="font-bold text-lg rounded-xs bg-green-300/20 px-2 py-1">{{ data?.company?.contactEmail }}</span>
+          <span
+            v-if="isLoading"
+            class="h-2 w-28 rounded bg-stone-400"
+          />
+          <span
+            v-else
+            class="font-bold text-lg rounded-xs bg-green-300/20 px-2 py-1"
+            >{{ data?.company?.contactEmail }}</span
+          >
           <span class="font-bold text-2xl">Contact Phone:</span>
-          <span v-if="isLoading" class="h-2 w-28 rounded bg-stone-400" />
-          <span v-else class="font-bold text-lg rounded-xs bg-green-300/20 px-2 py-1">{{ data?.company?.contactPhone }}</span>
+          <span
+            v-if="isLoading"
+            class="h-2 w-28 rounded bg-stone-400"
+          />
+          <span
+            v-else
+            class="font-bold text-lg rounded-xs bg-green-300/20 px-2 py-1"
+            >{{ data?.company?.contactPhone }}</span
+          >
         </p>
       </template>
     </JobCardWrapper>
     <JobCardWrapper class="details__description">
       <template #mainDetails>
         <span class="font-bold text-xl text-green-500">Job Description</span>
-        <div v-if="isLoading" class="h-2 w-28 rounded bg-stone-400" />
-        <span v-else class="text-lg">{{ data?.description }}</span>
+        <div
+          v-if="isLoading"
+          class="h-2 w-28 rounded bg-stone-400"
+        />
+        <span
+          v-else
+          class="text-lg"
+          >{{ data?.description }}</span
+        >
         <span class="font-bold text-xl text-green-500">Salary</span>
-        <div v-if="isLoading" class="h-2 w-28 rounded bg-stone-400" />
-        <span v-else class="text-lg">{{ data?.salary }}</span>
+        <div
+          v-if="isLoading"
+          class="h-2 w-28 rounded bg-stone-400"
+        />
+        <span
+          v-else
+          class="text-lg"
+          >{{ data?.salary }}</span
+        >
       </template>
     </JobCardWrapper>
     <JobCardWrapper class="details__buttons">
@@ -75,17 +130,17 @@ const { data, isLoading } = useGetJobById({ jobId: params.id as string })
 .details {
   @apply container
     grid
-    grid-cols-[auto_minmax(30em,_1fr)]
+    grid-cols-[minmax(60em,_1fr)_minmax(30em,_1fr)]
     grid-rows-[masonry]
     gap-4
     mx-auto
     py-8;
 
   grid-template-areas:
-    "main company"
-    "description company"
-    ". company"
-    ". buttons";
+    'main company'
+    'description company'
+    '. company'
+    '. buttons';
 
   .details__main {
     grid-area: main;
