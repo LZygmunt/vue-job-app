9161dbfd5b761359d86ea37e74b041fc0251426c
LZygmunt <lzygmunt0@gmail.com>
2025-04-02 18:58:23 +0200

    feat(JobAddView): invalidate job list query after successfully added job

--- src/api/useAddJob.ts
+++ src/api/useAddJob.ts
@@ -1,8 +1,17 @@
+import { useQueryClient } from '@tanstack/vue-query'
 import type { JobOfferDTO } from './jobDTO.ts'
 import usePost from './usePost.ts'
 
 const ENDPOINT = '/jobs' as const
 
-const useAddJob = () => usePost<Omit<JobOfferDTO, 'id'>, typeof ENDPOINT, JobOfferDTO>(ENDPOINT)
+const useAddJob = () => {
+  const qc = useQueryClient()
+
+  return usePost<Omit<JobOfferDTO, 'id'>, typeof ENDPOINT, JobOfferDTO>(ENDPOINT, {
+    onSuccess() {
+      qc.invalidateQueries({ queryKey: ['/jobs'] })
+    },
+  })
+}
 
 export default useAddJob
