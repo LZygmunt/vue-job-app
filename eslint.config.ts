f2a984c9a5df756fe19159deeaba45e26d91f48e
LZygmunt <lzygmunt0@gmail.com>
2025-04-25 19:14:25 +0200

    chore: add import sorter to eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,10 @@
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import pluginImport from 'eslint-plugin-import'
+import pluginVue from 'eslint-plugin-vue'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -22,11 +25,47 @@ export default defineConfigWithVueTs(
   pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
   {
+    plugins: {
+      import: pluginImport,
+    },
     rules: {
-      "arrow-body-style": ["error"],
-      "vue/attribute-hyphenation": ["error", 'never'],
-      "vue/v-on-event-hyphenation": ["error", 'never']
-    }
+      'arrow-body-style': ['error'],
+      'vue/attribute-hyphenation': ['error', 'never'],
+      'vue/v-on-event-hyphenation': ['error', 'never'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: '?@vue*|@vite',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '#/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['vue'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+        },
+      ],
+    },
   },
   {
     ...pluginVitest.configs.recommended,
